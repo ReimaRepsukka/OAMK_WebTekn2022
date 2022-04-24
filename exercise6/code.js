f455540c77a017f568d67baf9a555c24a191c230
repsukka <81526112+repsukka@users.noreply.github.com>
2022-04-24 19:39:58 +0300

    Comments

--- exercise6/code.js
+++ exercise6/code.js
@@ -1,3 +1,5 @@
+//Lisätään form-elementtiin submit kuuntelija
+//Eventin aiheuttaa napin painallus formin sisällä
 document.querySelector("form").addEventListener("submit", calc);
 
 
@@ -5,17 +7,23 @@ document.querySelector("form").addEventListener("submit", calc);
  * @param {Event} event 
  */
 function calc(event){
+    //Estetään oletustoiminta (sivun päivitys)
     event.preventDefault();
 
+    //Luodaan form data -olio form-elementistä eli tämän eventin targetista
     let formData = new FormData(event.currentTarget);
 
+    //Haetaan numerot (valuet) formista nimen perusteella
     let num1 = formData.get("num1");
     let num2 = formData.get("num2");
 
+    //Haetaan result div
     let res = document.getElementById("result");
 
+    //Haetaan formin radiobutton value
     let arit = formData.get("arit");
 
+    //Riippuen radiobuttonin arvosta lasketaan numeroiden summa tai erotus.
     if(arit == "sum"){
         res.textContent = "Lukujen summa on " + (Number(num1) + Number(num2));
     }else{
